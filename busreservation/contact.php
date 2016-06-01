5a9aa71faaab965ff6e5ddb4cdba32d66aafe3ca
fredrick oluoch <menty44@gmail.com>
2016-06-01 14:30:47 +0300

    first upload for bus booking system

---
+++ busreservation/contact.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Welcome Bus booking application</title>
+<link rel="stylesheet" type="text/css" href="xres/css/style.css" />
+<link rel="icon" type="image/png" href="xres/images/favicon.png" />
+<link type="text/css" href="css/styles.css" rel="stylesheet" media="all" />
+</head>
+
+<body>
+<div id="wrapper">
+	<div id="header">
+    <h1><a href="index.php"><img src="xres/images/logo.png" class="logo" alt="Bus booking software" /></a></h1>
+        <ul id="mainnav">
+			<li><a href="index.php">Home</a></li>
+           <!-- <li><a href="gallery.php">Gallery</a></li>-->
+            <!--<li><a href="history.php">History</a></li>-->
+            <li><a href="routes.php">Routes</a></li>
+            <li><a href="location.php">location</a></li>
+            <li class="current"><a href="contact.php">Contact Us</a></li>
+    	</ul>
+	</div>
+    <div id="content">
+    	<div id="gallerycontainer">
+			<div class="portfolio-area" style="margin:0 auto; padding:140px 20px 20px 20px; width:820px;">	
+				<div id="contactleft">
+					 BUS BOOKING SOFTWARE<br>
+( FLORIDA TRANSPORT, INCORPORATED)<br>
+NAIROBI KENYA<br>
+Phone:  0720106420<br><br>
+
+FLORIDA BUS<br>
+(FLORIDA TRANSPORT, INCORPORATED)<br>
+KISUMU,KENYA<br>
+Florida Bus Co., NAIROBI<br>
+
+Telephone No: 0720106420<br><br>
+
+
+Contact Numbers:<br>
+Florida Bus Company MOMBASA<br>
+0720106420<br>
+				</div><br>
+				<div id="contactright">
+					<h3>Message Form</h3>
+					<form class="validate" action="messageexec.php" method="POST">
+                        <p>
+                            <label for="name" class="required label">Name:</label><br>
+                            <input id="name" class="contactform" type="text" name="name" />
+                        </p>
+                        <p>
+                            <label for="email" class="required label">Email:</label><br>
+                            <input id="email" class="contactform" placeholder="Example: john@doe.com" type="text" name="email" />
+                        </p>
+                        <p>
+                            <label for="subject" class="required label">Subject:</label><br>
+                            <input id="subject" class="contactform" type="text" name="subject" />
+                        </p>
+                        <p>
+                            <label id="message-label" for="message" class="required label">Message:</label><br>
+                            <textarea id="message" class="contactform" name="message" cols="28" rows="5"></textarea>
+                        </p>
+                        <p>
+                            <label></label>
+                            <input class="contactform" id="submit-button" type="submit" name="Submit" value="Submit" style="height: 35px;" />
+                        </p>
+                    </form>
+				</div>
+               	<div class="column-clear"></div>
+            </div>
+			<div class="clearfix"></div>
+        </div>
+    </div>
+    
+
+
+
+<div id="footer">
+	<h4>0720106420 &bull; <a href="contact-us.php">View park towers 15th floor g.e.c   </a></h4>
+	<p>Hours of Operation&nbsp;&nbsp;&bull;&nbsp;&nbsp;Mon - Sun: 10:00 am - 12:00 am</p>
+	<a href="index.php"><img src="xres/images/footer-logo.jpg" alt="James Buchanan Pub and Restaurant" /></a>
+	<p>&copy; Copyright 2013 arif techonlogies Bus booking software | All Rights Reserved <br /></p>
+</div>
+
+</div>
+</body>
+</html>
